de5d6740ae7a611ceb84d6fdf323ca681dc0064f
Jon Mikoš <atletjm@gmail.com>
2021-11-16 20:47:07 +0100

    Črna log magija

--- Dodatne/horner.m
+++ Dodatne/horner.m
@@ -1,7 +1,18 @@
-function x = horner(p, a)
-    a = float(a);
-    x = float(p(1));
+function x = horner(p, a, mantissa_length, base)
+    
+    switch nargin
+        case 2
+            mantissa_length = 53;
+            base = 2;
+        case 3
+            base = 2;
+    end
+
+    a = float(a, mantissa_length, base);
+    x = float(p(1), mantissa_length, base);
     for k = 2:length(p)
-        x = float(float(x * a) + float(p(k)));
+        xa = float(x * a, mantissa_length, base);
+        pk = float(p(k), mantissa_length, base);
+        x = float(xa + pk, mantissa_length, base);
     end
 end
