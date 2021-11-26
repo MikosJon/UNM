905d4533b490b7682381a1b240bedac2898738ca
Jon Mikoš <atletjm@gmail.com>
2021-11-26 10:03:48 +0100

    mal drugi Ridder

--- Dodatne/Ridder.m
+++ Dodatne/Ridder.m
@@ -4,21 +4,26 @@ function [nicla, X, k] = Ridder(f, a, b, eps, koraki)
     end
     X = a;
     for k = 2 : koraki
+
+        if abs(b-a) < eps
+            break
+        end
+
         c = a + (b - a) / 2;
         x = c + (c - a) * (sign(f(a) - f(b)) * f(c)) / sqrt(f(c)^2 - f(a) * f(b));
 
         X(k) = x;
 
-        if abs(X(k) - X(k-1)) < eps
-            break
-        end
+%         if abs(X(k) - X(k-1)) < eps
+%             break;
+%         end
 
         sx = sign(f(x));
         sc = sign(f(c));
         sa = sign(f(a));
         if sx ~= sc
-            a = x;
-            b = c;
+            a = c;
+            b = x;
         elseif sx ~= sa
             b = x;
         else
@@ -26,6 +31,8 @@ function [nicla, X, k] = Ridder(f, a, b, eps, koraki)
         end
     end
 
-    nicla = X(k);
-    k = k - 1;
+    nicla = X(k-1);
+    k = k - 2;
+%     nicla = X(k);
+%     k = k - 1;
 end
--- testni_kviz_3.m
+++ testni_kviz_3.m
@@ -4,7 +4,8 @@ f = @(x) exp(x) * sin(11 * exp(x));
 disp(X(3));
 
 [~, X, ~] = Ridder(f, 0.9, 1, 1e-14, 100);
-disp(length(X) - 2);
+disp(length(X) - 3);
+% disp(length(X) - 2);
 
 [n1, X1, k1] = Ridder(f, 0.5, 0.6, 1e-10, 100);
 [~, ~, k2] = bisekcija(f, 0.5, 0.6, 1e-10, 100);
